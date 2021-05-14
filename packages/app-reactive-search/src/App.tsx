a82b9ce5617cee2b91e21ee8f87450c5c6c902d6
Emanuel Schmitt <emanuel.schmitt@me.com>
2021-05-14 19:12:53 +0200

    feat: basic flexbox

--- packages/app-reactive-search/src/App.tsx
+++ packages/app-reactive-search/src/App.tsx
@@ -12,69 +12,90 @@ import {
 function App() {
   return (
     <ReactiveBase url="http://localhost:3000" app="lazydigger">
-      <SingleDropdownList
-        componentId="Genre"
-        dataField="genres"
-        showCount
-        URLParams
-        title="Genres"
-        react={{
-          and: ['Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
         }}
-      />
-      <MultiDropdownList
-        componentId="Styles"
-        dataField="styles"
-        showCount
-        URLParams
-        title="Styles"
-        react={{
-          and: ['Genre', 'Country', 'RatingAverage', 'Rareness', 'Price'],
-        }}
-        showFilter
-      />
-      <SingleDropdownList
-        componentId="Country"
-        dataField="country"
-        showCount
-        URLParams
-        title="Country"
-        react={{
-          and: ['Genre', 'Styles', 'RatingAverage', 'Rareness', 'Price'],
-        }}
-      />
-      <RangeSlider
-        componentId="RatingAverage"
-        dataField="averageRating"
-        title="Average Rating"
-        range={{
-          start: 0,
-          end: 500,
-        }}
-        react={{
-          and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price'],
-        }}
-      />
-      <DynamicRangeSlider componentId="Rareness" dataField="rareness" title="Rareness" />
-      <DynamicRangeSlider componentId="Price" dataField="lowest_price" title="Price" />
-      <SelectedFilters />
-      <ReactiveList
-        componentId="SearchResult"
-        dataField="id"
-        pagination
-        URLParams
-        size={20}
-        renderItem={(res: any) => (
-          <div key={res.id}>
-            {res.artists.map((artist: any) => artist.name).join(', ')}
-            {' - '}
-            {res.title}
-          </div>
-        )}
-        react={{
-          and: ['Genre', 'Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
-        }}
-      />
+      >
+        <div
+          style={{
+            minWidth: 400,
+            padding: '32px',
+          }}
+        >
+          <SingleDropdownList
+            componentId="Genre"
+            dataField="genres"
+            showCount
+            URLParams
+            title="Genres"
+            react={{
+              and: ['Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
+            }}
+          />
+          <MultiDropdownList
+            componentId="Styles"
+            dataField="styles"
+            showCount
+            URLParams
+            title="Styles"
+            react={{
+              and: ['Genre', 'Country', 'RatingAverage', 'Rareness', 'Price'],
+            }}
+            showFilter
+          />
+          <SingleDropdownList
+            componentId="Country"
+            dataField="country"
+            showCount
+            URLParams
+            title="Country"
+            react={{
+              and: ['Genre', 'Styles', 'RatingAverage', 'Rareness', 'Price'],
+            }}
+          />
+          <RangeSlider
+            componentId="RatingAverage"
+            dataField="averageRating"
+            title="Average Rating"
+            range={{
+              start: 0,
+              end: 500,
+            }}
+            react={{
+              and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price'],
+            }}
+          />
+          <DynamicRangeSlider componentId="Rareness" dataField="rareness" title="Rareness" />
+          <DynamicRangeSlider componentId="Price" dataField="lowest_price" title="Price" />
+        </div>
+        <div
+          style={{
+            flexGrow: 1,
+            padding: '32px',
+          }}
+        >
+          <SelectedFilters />
+          <ReactiveList
+            componentId="SearchResult"
+            dataField="id"
+            pagination
+            URLParams
+            size={20}
+            renderItem={(res: any) => (
+              <div key={res.id}>
+                {res.artists.map((artist: any) => artist.name).join(', ')}
+                {' - '}
+                {res.title}
+              </div>
+            )}
+            react={{
+              and: ['Genre', 'Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
+            }}
+          />
+        </div>
+      </div>
     </ReactiveBase>
   );
 }
