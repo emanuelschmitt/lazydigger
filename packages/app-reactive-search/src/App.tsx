5b4634f9cf5cb07afa2154ece3bc0322adc1d280
Emanuel Schmitt <emanuel.schmitt@me.com>
2021-05-15 12:36:49 +0200

    feat: add material ui and clean up

--- packages/app-reactive-search/src/App.tsx
+++ packages/app-reactive-search/src/App.tsx
@@ -8,6 +8,18 @@ import {
   DynamicRangeSlider,
   SelectedFilters,
 } from '@appbaseio/reactivesearch';
+import {
+  Grid,
+  Table,
+  TableCell,
+  TableRow,
+  TableBody,
+  TableHead,
+  TableContainer,
+  TablePagination,
+  Paper,
+  Link,
+} from '@material-ui/core';
 
 function App() {
   return (
@@ -20,67 +32,98 @@ function App() {
       >
         <div
           style={{
-            minWidth: 400,
+            maxWidth: 400,
             padding: '32px',
+            background: '#ddd',
           }}
         >
-          <SingleDropdownList
-            componentId="Genre"
-            dataField="genres"
-            showCount
-            URLParams
-            title="Genres"
-            react={{
-              and: ['Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
-            }}
-          />
-          <MultiDropdownList
-            componentId="Styles"
-            dataField="styles"
-            showCount
-            URLParams
-            title="Styles"
-            react={{
-              and: ['Genre', 'Country', 'RatingAverage', 'Rareness', 'Price'],
-            }}
-            showFilter
-          />
-          <SingleDropdownList
-            componentId="Country"
-            dataField="country"
-            showCount
-            URLParams
-            title="Country"
-            react={{
-              and: ['Genre', 'Styles', 'RatingAverage', 'Rareness', 'Price'],
-            }}
-          />
-          <RangeSlider
-            componentId="RatingAverage"
-            dataField="averageRating"
-            title="Average Rating"
-            range={{
-              start: 0,
-              end: 500,
-            }}
-            react={{
-              and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price'],
-            }}
-          />
-          <DynamicRangeSlider componentId="Rareness" dataField="rareness" title="Rareness" />
-          <DynamicRangeSlider title="Price" componentId="Price" dataField="lowest_price" />
-          <RangeSlider
-            componentId="Year"
-            dataField="year"
-            title="Year"
-            range={{
-              start: 1920,
-              end: new Date().getFullYear(),
-            }}
-            react={{
-              and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price'],
-            }}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <SingleDropdownList
+                componentId="Genre"
+                dataField="genres"
+                showCount
+                URLParams
+                title="Genres"
+                react={{
+                  and: ['Styles', 'Country', 'RatingAverage', 'Rareness', 'Price', 'RatingAverage'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <MultiDropdownList
+                componentId="Styles"
+                dataField="styles"
+                showCount
+                URLParams
+                title="Styles"
+                react={{
+                  and: ['Genre', 'Country', 'RatingAverage', 'Rareness', 'Price', 'RatingAverage'],
+                }}
+                showFilter
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <SingleDropdownList
+                componentId="Country"
+                dataField="country"
+                showCount
+                URLParams
+                title="Country"
+                react={{
+                  and: ['Genre', 'Styles', 'RatingAverage', 'Rareness', 'Price', 'RatingAverage'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <RangeSlider
+                componentId="RatingAverage"
+                dataField="averageRating"
+                title="Average Rating"
+                range={{
+                  start: 0,
+                  end: 500,
+                }}
+                react={{
+                  and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <DynamicRangeSlider
+                componentId="Rareness"
+                dataField="rareness"
+                title="Rareness"
+                react={{
+                  and: ['Genre', 'Styles', 'Country', 'Price', 'RatingAverage'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <DynamicRangeSlider
+                title="Price"
+                componentId="Price"
+                dataField="lowest_price"
+                react={{
+                  and: ['Genre', 'Styles', 'Country', 'Rareness', 'RatingAverage'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <RangeSlider
+                componentId="Year"
+                dataField="year"
+                title="Year"
+                range={{
+                  start: 1920,
+                  end: new Date().getFullYear(),
+                }}
+                react={{
+                  and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price', 'RatingAverage'],
+                }}
+              />
+            </Grid>
+          </Grid>
         </div>
         <div
           style={{
@@ -88,24 +131,81 @@ function App() {
             padding: '32px',
           }}
         >
-          <SelectedFilters />
-          <ReactiveList
-            componentId="SearchResult"
-            dataField="id"
-            pagination
-            URLParams
-            size={20}
-            renderItem={(res: any) => (
-              <div key={res.id}>
-                {res.artists.map((artist: any) => artist.name).join(', ')}
-                {' - '}
-                {res.title}
-              </div>
-            )}
-            react={{
-              and: ['Genre', 'Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
-            }}
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <SelectedFilters />
+            </Grid>
+            <Grid item xs={12}>
+              <Paper>
+                <ReactiveList
+                  componentId="SearchResult"
+                  dataField="id"
+                  pagination
+                  URLParams
+                  showResultStats={false}
+                  size={25}
+                  render={(data) => {
+                    return (
+                      <TableContainer>
+                        <Table>
+                          <TableHead>
+                            <TableRow>
+                              <TableCell>Artists</TableCell>
+                              <TableCell>Title</TableCell>
+                              <TableCell>Country</TableCell>
+                              <TableCell>Year</TableCell>
+                              <TableCell>Genres</TableCell>
+                              <TableCell>Rating</TableCell>
+                              <TableCell>Rareness</TableCell>
+                              <TableCell>Price</TableCell>
+                              <TableCell></TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {data.data.map((res: any) => (
+                              <TableRow key={res.id}>
+                                <TableCell>{res.artists.map((artist: any) => artist.name).join(', ')}</TableCell>
+                                <TableCell>{res.title}</TableCell>
+                                <TableCell>{res.country}</TableCell>
+                                <TableCell>{res.year}</TableCell>
+                                <TableCell>{res.genres.join(', ')}</TableCell>
+                                <TableCell>
+                                  {res.community.rating.average} (#{res.community.rating.count})
+                                </TableCell>
+                                <TableCell>{(res.rareness / 100).toFixed(2)}</TableCell>
+                                <TableCell>{(res.price / 100).toFixed(2)}</TableCell>
+                                <TableCell>
+                                  <Link href={res.uri} target="_blank">
+                                    View
+                                  </Link>
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                    );
+                  }}
+                  renderNoResults={() => null}
+                  renderPagination={({ totalPages, currentPage, setPage }) => (
+                    <TablePagination
+                      count={totalPages * 10}
+                      rowsPerPage={10}
+                      page={currentPage}
+                      onChangePage={(_, value) => {
+                        setPage(value);
+                      }}
+                      rowsPerPageOptions={[10]}
+                      component="div"
+                    />
+                  )}
+                  react={{
+                    and: ['Genre', 'Styles', 'Country', 'RatingAverage', 'Rareness', 'Price'],
+                  }}
+                />
+              </Paper>
+            </Grid>
+          </Grid>
         </div>
       </div>
     </ReactiveBase>
