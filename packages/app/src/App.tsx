16ca2402748f4aad4d5fafd01e07845755a7bad2
Emanuel Schmitt <emanuel.schmitt@me.com>
2021-05-16 08:35:56 +0200

    feat: sorting improvements

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,10 +2,10 @@ import './App.css';
 import {
   MultiDropdownList,
   RangeSlider,
+  SingleRange,
   ReactiveBase,
   ReactiveList,
   SingleDropdownList,
-  DynamicRangeSlider,
   SelectedFilters,
 } from '@appbaseio/reactivesearch';
 import {
@@ -78,25 +78,17 @@ function App() {
               />
             </Grid>
             <Grid item xs={12}>
-              <RangeSlider
-                componentId="RatingAverage"
-                dataField="averageRating"
-                title="Average Rating"
-                URLParams
-                range={{
-                  start: 0,
-                  end: 500,
-                }}
-                react={{
-                  and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price', 'Year'],
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <DynamicRangeSlider
+              <SingleRange
                 componentId="Rareness"
                 dataField="rareness"
                 title="Rareness"
+                showRadio
+                data={[
+                  { start: 0, end: 100, label: 'Common' },
+                  { start: 100, end: 200, label: 'Uncommon' },
+                  { start: 200, end: 500, label: 'Rare' },
+                  { start: 500, end: 100000, label: 'Ultra Rare' },
+                ]}
                 URLParams
                 react={{
                   and: ['Genre', 'Styles', 'Country', 'Price', 'RatingAverage', 'Year'],
@@ -104,13 +96,38 @@ function App() {
               />
             </Grid>
             <Grid item xs={12}>
-              <DynamicRangeSlider
+              <SingleRange
                 title="Price"
                 componentId="Price"
                 dataField="lowest_price"
+                showRadio
                 URLParams
+                data={[
+                  { start: 0, end: 10, label: '0 - 10 EUR' },
+                  { start: 10, end: 25, label: '10 - 25 EUR' },
+                  { start: 25, end: 50, label: '25 - 50 EUR' },
+                  { start: 50, end: 75, label: '50 - 75 EUR' },
+                  { start: 75, end: 100, label: '75 - 100 EUR' },
+                  { start: 100, end: 200, label: '100 - 200 EUR' },
+                  { start: 200, end: 1000000, label: '> 200 EUR' },
+                ]}
                 react={{
                   and: ['Genre', 'Styles', 'Country', 'Rareness', 'RatingAverage', 'Year'],
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <RangeSlider
+                componentId="RatingAverage"
+                dataField="averageRating"
+                title="Average Rating"
+                URLParams
+                range={{
+                  start: 400,
+                  end: 500,
+                }}
+                react={{
+                  and: ['Genre', 'Styles', 'Country', 'Rareness', 'Price', 'Year'],
                 }}
               />
             </Grid>
@@ -149,6 +166,12 @@ function App() {
                   pagination
                   URLParams
                   showResultStats={false}
+                  sortOptions={[
+                    { label: 'Relevance', dataField: 'id', sortBy: 'desc' },
+                    { label: 'Rareness', dataField: 'rareness', sortBy: 'desc' },
+                    { label: 'Price', dataField: 'lowest_price', sortBy: 'desc' },
+                    { label: 'Rating', dataField: 'averageRating', sortBy: 'desc' },
+                  ]}
                   size={PAGE_SIZE}
                   render={(data) => {
                     return (
